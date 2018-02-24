7feb00629c99351895a5b66bb1defa4269e24990
Simon Wittber <simonwittber@unity3d.com>
2018-02-24 19:14:56 +0000

    lock free queue

---
+++ Collections/LockFreeFIFO.cs
@@ -0,0 +1,47 @@
+namespace DifferentMethods.Extensions.Collections
+{
+    /// <summary>
+    /// Lock free Queue, as in Single Writer, Single Reader.
+    /// </summary>
+    public class LockFreeQueue<T>
+    {
+        volatile int read, write;
+        T[] buffer;
+
+        public LockFreeQueue(int bufferSize)
+        {
+            buffer = new T[bufferSize];
+        }
+
+        public bool Dequeue(out T item)
+        {
+            if (read == write)
+            {
+                item = default(T);
+                return false;
+            }
+            item = buffer[read];
+            if ((read + 1) >= buffer.Length)
+                read = 0;
+            else
+                read = read + 1;
+            return true;
+        }
+
+        public bool Enqueue(T item)
+        {
+            int newidx;
+            if ((write + 1) >= buffer.Length)
+                newidx = 0;
+            else
+                newidx = write + 1;
+            if (newidx == read)
+                return false;
+            buffer[write] = item;
+            write = newidx;
+            return true;
+        }
+
+    }
+}
+
