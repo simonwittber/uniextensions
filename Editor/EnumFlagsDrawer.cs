2ed40944f19036221d22363bc82a9630ee72f3f2
Simon Wittber <simonwittber@unity3d.com>
2018-05-21 16:27:38 +0100

    enumflags + VectorInt extensions

---
+++ Editor/EnumFlagsDrawer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+
+[CustomPropertyDrawer(typeof(EnumFlagsAttribute))]
+public class EnumFlagsDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+
+        var flagSettings = (EnumFlagsAttribute)attribute;
+        var targetEnum = (Enum)Enum.ToObject(fieldInfo.FieldType, property.intValue);
+
+        var propName = flagSettings.name;
+        if (string.IsNullOrEmpty(propName))
+            propName = ObjectNames.NicifyVariableName(property.name);
+
+        EditorGUI.BeginChangeCheck();
+        EditorGUI.BeginProperty(position, label, property);
+
+        var enumNew = EditorGUI.EnumFlagsField(position, propName, targetEnum);
+
+        if (!property.hasMultipleDifferentValues || EditorGUI.EndChangeCheck())
+            property.intValue = (int)Convert.ChangeType(enumNew, targetEnum.GetType());
+
+        EditorGUI.EndProperty();
+    }
+}
--- Extensions/Vector3ExtensionMethods.cs
+++ Extensions/Vector3ExtensionMethods.cs
@@ -19,4 +19,52 @@ public static class Vector3ExtensionMethods
         return (origin - test).sqrMagnitude < (maxDistance * maxDistance);
     }
 
+    public static Vector3 Round(this Vector3 vector)
+    {
+        vector.x = Mathf.Round(vector.x);
+        vector.y = Mathf.Round(vector.y);
+        vector.z = Mathf.Round(vector.z);
+        return vector;
+    }
+
+    public static Vector3 Floor(this Vector3 vector)
+    {
+        vector.x = Mathf.Floor(vector.x);
+        vector.y = Mathf.Floor(vector.y);
+        vector.z = Mathf.Floor(vector.z);
+        return vector;
+    }
+
+    public static Vector3Int FloorToInt(this Vector3 vector)
+    {
+        var v = Vector3Int.zero;
+        v.x = Mathf.FloorToInt(vector.x);
+        v.y = Mathf.FloorToInt(vector.y);
+        v.z = Mathf.FloorToInt(vector.z);
+        return v;
+    }
+
+    public static Vector3 Abs(this Vector3 vector)
+    {
+        vector.x = Mathf.Abs(vector.x);
+        vector.y = Mathf.Abs(vector.y);
+        vector.z = Mathf.Abs(vector.z);
+        return vector;
+    }
+
+    public static Vector2 xy(this Vector3 vector)
+    {
+        return new Vector2(vector.x, vector.y);
+    }
+
+    public static Vector2 xz(this Vector3 vector)
+    {
+        return new Vector2(vector.x, vector.z);
+    }
+
+    public static Vector2 yz(this Vector3 vector)
+    {
+        return new Vector2(vector.y, vector.z);
+    }
+
 }
